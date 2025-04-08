3cc876823786c7d4b67961b184222addf6d72bb1
Ruan Marcos <ruanmbalbino@gmail.com>
2025-04-08 17:05:21 -0300

    some fixes

---
+++ .scripts/aomosso.py
@@ -0,0 +1,70 @@
+message = """
+Bom dia clientes abençoados
+SEGUE O CARDÁPIO DE DELIVERY DO VILLA  17/10
+
+SUGESTÃO DO DIA: ARROZ CARRETEIRO
+(Arroz Carreteiro, Mandioca Frita, Couve e Ovo Frito) 
+Nos tamanhos: 
+Média- R$ 17,00
+Grande- R$ 19,00
+Extra-Grande-  R$ 21,00 
+
+OU
+
+COMBO ARROZ CARRETEIRO
+*Arroz Carreteiro, Mandioca Frita, Couve e  2 ovos fritos.
+Serve 2 a 3 pessoas. Ideal para almoço em família e com amigos. 
+55,00
+
+CARDÁPIO DO DIA
+Para uma melhor acomodação e satisfação de você, cliente,  estabelecemos uma quantidade para a montagem das marmitex. Sendo: até 4 itens na guarnição, até 8 itens na saladas, e 1 opção de proteína.. Contamos e agradecemos a sua compreensão 🥰🧡🙏🏻
+
+GUARNIÇÕES: 
+Arroz ou
+Arroz Integral 
+
+ Feijão ou
+Tutu de Feijão (Com Couve e sem Carne)
+
+Farofa (Sem Carne)
+Revirado de Ervilha (Contém Ovo)
+
+Ensopadinho de Batata ou
+Espaguete ao Alho e Óleo com Ervas
+
+Batata Chips ou
+Mandioca Frita
+
+SALADAS 🥗🍅🥦🥕
+Alface
+Rúcula
+Beterraba Cozida
+Cenoura Cozida
+Brócolis
+Couve Flor
+Repolho Temperado
+Vagem
+Chuchu
+Batata Doce
+Salpicão (Contém Frango)
+Batata Salsa
+Abobrinha com Mostarda e Mel
+Berinjela
+Jiló
+Mugango
+Pepino
+Tomate
+Grão de Bico
+
+PROTEÍNA
+Filé de Frango Grelhado, Bife de Fígado, Linguiça de Porco, Bife de Lombo, Carne Moida Refogada, Isca de vaca em Tiras, 
+
+Ovo Frito,  Omelete de Queijo, Omelete Completa com Legumes, Omelete de Atum com Queijo,
+
+Proteína Vegana: Falafel Vegano de Grão de Bico Assado @Veg-li.
+
+Por favor informar se deseja proteína com cebola
+
+Obs: O cardápio do Self-Service pode conter mais opções. Venha Conferir!  😋🍽️💕
+"""
+
